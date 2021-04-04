2f976411d3eb77f38b8dc64e308d2b038d856ead
Ririsgultom <79890624+Ririsgultom@users.noreply.github.com>
2021-04-04 12:06:22 +0700

    membuat file index.php untuk merancang formulir.

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>form pendaftaran</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="wrap">
+		<div class="container">
+	<h1>Form Pendaftaran</h1>
+	<form action="form_add_aksi.php" method="POST">
+		<table>
+			<tr>
+				<td>Nama Lengkap</td>
+				<td>:</td>
+				<td><input type="text" name="nama_lengkap"></td>
+			</tr>
+			<tr>
+				<td>Email</td>
+				<td>:</td>
+				<td><input type="text" name="email"></td>
+			</tr>
+			<tr>
+				<td>Tempat Lahir</td>
+				<td>:</td>
+				<td><input type="text" name="tempat_lahir"></td>
+			</tr>
+			<tr>
+				<td>Tanggal Lahir</td>
+				<td>:</td>
+				<td><input type="date" name="tanggal_lahir"></td>
+			</tr>
+			<tr>
+				<td>Jenis Kelamin</td>
+				<td>:</td>
+				<td><input type="radio" name="jk" value="Laki-Laki">Laki-Laki
+				<input type="radio" name="jk" value="Perempuan">Perempuan
+				</td>
+			</tr>
+			<tr>
+				<td>No Telepon</td>
+				<td>:</td>
+				<td><input type="number" name="no_telepon"></td>
+			</tr>
+			<tr>
+				<td>Alamat</td>
+				<td>:</td>
+				<td><textarea name="alamat"></textarea></td>
+			</tr>
+			<tr>
+				<td>Pilih Organisasi</td>
+				<td>:</td>
+				<td><select name="pilih_organisasi">
+					<option>--Pilih Organisasi--</option>
+					<option>Kerohanian</option>
+					<option>Olahraga</option>
+					<option>Musik</option>
+					<option>Sains & Teknologi</option>
+					<option>Fotografi</option>
+					<option>Pramuka</option>
+				</select>
+			</td>
+		</tr>
+      
+      </table>
+	</form>
+</div>
+</div>
+</body>
+</html>
